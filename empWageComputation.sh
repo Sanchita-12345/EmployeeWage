1cf63d77687d89ffbc7805483b5774ddb20adcfd
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-06 18:46:03 -0800

    Employee wage program done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,11 @@
+#!/bin/bash -x
 echo "Welcome to the Employee wage program "
+isPresent=1
+checkAttendance=$((RANDOM%2))
+
+if [ $isPresent -eq $checkAttendance ]
+then
+	echo "Employee present"
+else
+	echo "Employee Absent"
+fi
