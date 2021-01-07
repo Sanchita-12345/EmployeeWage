70d4261cc28c2c95d35cf3d62b686ec54ed6d5e8
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-06 19:14:41 -0800

    Employee daily wage calculation done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to the Employee wage program "
 isPresent=1
+ratePerHour=20;
+noOfHours=8;
 checkAttendance=$((RANDOM%2))
 
 if [ $isPresent -eq $checkAttendance ]
 then
 	echo "Employee present"
+	payment=$(($ratePerHour*$noOfHours))
+	echo "Payment is :" $payment
+
 else
 	echo "Employee Absent"
+	echo "Payment is 0"
 fi
