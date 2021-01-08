a04c6b4ddaa824000be2ed412182771c818f03ae
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-07 19:47:52 -0800

    Switch case part done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -x
 echo "Welcome to the Employee wage program "
 isPresent=1
+isPartTime=2
 ratePerHour=20;
 noOfHours=8;
-checkAttendance=$((RANDOM%2))
+noOfPartTimeHours=4
+checkAttendance=$((RANDOM%3))
 
-if [ $isPresent -eq $checkAttendance ]
-then
-	echo "Employee present"
-	payment=$(($ratePerHour*$noOfHours))
-	echo "Payment is :" $payment
-
-else
-	echo "Employee Absent"
-	echo "Payment is 0"
-fi
+case $checkAttendance in
+	$isPresent)
+		echo "Employee present"
+		payment=$(($ratePerHour*$noOfHours))
+		echo "Payment is :" $payment
+		;;
+	$isPartTime)
+   	echo "Employee is PartTime"
+		payment=$(( $ratePerHour * $noOfPartTimeHours ))
+		echo "Payment is :" $payment
+		;;
+	*)
+		echo "Employee Absent"
+		echo "Payment is 0"
+		;;
+esac
