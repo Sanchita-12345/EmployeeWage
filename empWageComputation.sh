508e8dc42568aaf3395dd197b8cd33d05adf9601
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-07 20:41:15 -0800

    Wages in 100 hours and 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
 echo "Welcome to the Employee wage program "
 isPresent=1
-checkAttendance=$((RANDOM%2))
+isPartTime=2
+ratePerHour=20;
+noOfHours=8;
+noOfPartTimeHours=4
+workDay=20
+totalWorkHours=100
+checkAttendance=$((RANDOM%3))
 
-if [ $isPresent -eq $checkAttendance ]
-then
-	echo "Employee Present"
-else
-	echo "Employee Absent"
-fi
+case $checkAttendance in
+	$isPresent)
+		echo "Employee present"
+		payment=$(($ratePerHour*$noOfHours*$workDay*$totalWorkHours))
+		echo "Payment in a month is :" $payment
+		;;
+	$isPartTime)
+   	echo "Employee is PartTime"
+		payment=$(($ratePerHour*$noOfPartTimeHours*$workDay*$totalWorkHours))
+		echo "Payment in a month is :" $payment
+		;;
+	*)
+		echo "Employee Absent"
+		echo "Payment in a month is 0"
+		;;
+esac
