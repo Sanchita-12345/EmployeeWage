89f7d56ee32038b265875ada87d303b70b740785
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-07 22:23:51 -0800

    Day , Daily Wage and Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,39 @@
 #!/bin/bash -x
 echo "Welcome to the Employee wage program "
 isPresent=1
-checkAttendance=$((RANDOM%2))
+isPartTime=2
+ratePerHour=20
+noOfHours=8
+noOfPartTimeHours=4
+workDay=20
+totalWorkHours=100
+checkAttendance=$((RANDOM%3))
 
-if [ $isPresent -eq $checkAttendance ]
-then
-	echo "Employee Present"
-else
-	echo "Employee Absent"
-fi
+function getWorkHour() {
+
+	case $checkAttendance in
+		$isPresent)
+			echo "Employee present"
+			dailyWage=$(($ratePerHour*$noOfHours))
+			echo "Daily Wagee is : " $dailyWage
+			totalWage=$(($ratePerHour*$noOfHours*$workDay*$totalWorkHours))
+			echo "Total Wage is :" $totalWage
+			echo "Day is : " $workDay
+			;;
+		$isPartTime)
+   		echo "Employee Part Time"
+         dailyWage=$(($ratePerHour*$noOfPartTimeHours))
+         echo "Daily Wage is : " $dailyWage
+         totalWage=$(($ratePerHour*$noOfPartTimeHours*$workDay*$totalWorkHours))
+         echo "Total Wage is :" $totalWage
+			echo "Day is : " $workDay
+         ;;
+		*)
+			echo "Employee is absent"
+			echo "Daily wage is 0"
+			echo "Total wage is 0"
+			echo "Day is 0"
+			;;
+	esac
+}
+getWorkHour
